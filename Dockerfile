f755d7147bc2b58b3d4860546d1495bf4f013c96
cmyiyi <cmygit@163.com>
2019-10-20 16:48:41 +0800

    change jenkins pipeline.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM java:8-alpine
 
 MAINTAINER cmy yi177377@163.com
 
-ADD target/*.jar app.jar
+ADD ./target/*.jar app.jar
 
-EXPOSE 8761
+EXPOSE 8080
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # 打包镜像
-docker build -t registry.cn-hangzhou.aliyuncs.com/yiyi-springcloud/eureka .
+echo "build image"
+docker build -t registry.cn-hangzhou.aliyuncs.com/yiyi-jenkins/jenkins-demo1 .
 
 # 查看镜像
+echo "show images"
 docker images
 
 # 启动容器
-echo "start container"
+#echo "start container"
 #docker run -d -p 8761:8080 --name jenkins-demo1 registry.cn-hangzhou.aliyuncs.com/yiyi-jenkins/demo1
 
