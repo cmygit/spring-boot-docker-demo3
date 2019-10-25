89bcf698a5c4a9f1a82d48d1991d31af7ebc344c
cmyiyi <cmygit@163.com>
2019-10-25 15:27:15 +0800

    change jenkins pipeline. 112

--- docker-build.sh
+++ docker-build.sh
@@ -3,10 +3,10 @@
 ImageFullName=$Registry/$NameSpace/$ImageName
 
 echo "docker build..."
-ehco "ImageFullName: $ImageFullName"
+echo "ImageFullName: $ImageFullName"
 
 # 先删除之前的容器
-echo "remobe old container"
+echo "remobe old container..."
 docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
 
 ## 删除之前的镜像
@@ -14,19 +14,19 @@ docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
 #docker rmi $ImageFullName
 
 # 打包镜像
-echo "build image"
+echo "build image..."
 docker build -t $ImageFullName .
 
 # 打印当前镜像
-echo "current docker images"
+echo "current docker images..."
 docker images | grep $$ImageFullName
 
 # 启动容器
-echo "start container"
+echo "start container..."
 docker run -d -p 9090:8080 --name $ImageName $Registry/$NameSpace/$ImageName
 
 # 打印当前容器
-echo "current container"
+echo "current container..."
 docker ps -a | grep $ImageName
 
 echo "star container success!"
