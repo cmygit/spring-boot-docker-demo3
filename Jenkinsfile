ca9118f6fa14641fe6c9d344957fafcd3015f455
cmyiyi <cmygit@163.com>
2019-10-26 02:03:27 +0800

    change jenkins pipeline. 140

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,25 +5,23 @@ pipeline {
         Registry = 'registry.cn-hangzhou.aliyuncs.com'
         NameSpace = 'cmyiyi11'
         ImageName = 'ci-demo1'
-        DOCKER_USER_USR = credentials('docker-ali-registry-user')
-        DOCKER_USER_PSW = credentials('docker-ali-registry-user')
         DOCKER_USER_USERNAME = credentials('docker-ali-registry-user-username')
         DOCKER_USER_PASSWORD = credentials('docker-ali-registry-user-password')
     }
 
     stages {
-//        stage('maven-build') {
-//            agent {
-//                docker {
-//                    image 'maven:3-alpine'
-//                    args '-v $HOME/.m2:/root/.m2'
-//                }
-//            }
-//
-//            steps {
-//                sh 'mvn -B -DskipTests clean package'
-//            }
-//        }
+        stage('maven-build') {
+            agent {
+                docker {
+                    image 'maven:3-alpine'
+                    args '-v $HOME/.m2:/root/.m2'
+                }
+            }
+
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
 
         stage('docker-build') {
             agent any
@@ -34,12 +32,12 @@ pipeline {
             }
         }
 
-//        stage('clean') {
-//            agent any
-//
-//            steps {
-//                cleanWs(notFailBuild: true)
-//            }
-//        }
+        stage('clean') {
+            agent any
+
+            steps {
+                cleanWs(notFailBuild: true)
+            }
+        }
     }
 }
--- docker-build.sh
+++ docker-build.sh
@@ -2,36 +2,34 @@
 
 ImageFullName=$Registry/$NameSpace/$ImageName
 
-#echo "docker build..."
-#echo "ImageFullName: $ImageFullName"
-#
-## 先删除之前的容器
-#echo "remobe old container..."
-#docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
-#
-## 删除之前的镜像
-#echo "romove old image..."
-#docker rmi $ImageFullName
-#
-## 打包镜像
-#echo "build image..."
-#docker build -t $ImageFullName .
-#
-## 打印当前镜像
-#echo "current docker images..."
-#docker images | grep $ImageFullName
-#
-## 启动容器
-#echo "start container..."
-#docker run -d -p 9090:8080 --name $ImageName $ImageFullName
-#
-## 打印当前容器
-#echo "current container..."
-#docker ps -a | grep $ImageName
+echo "docker build..."
+echo "ImageFullName: $ImageFullName"
+
+# 先删除之前的容器
+echo "remobe old container..."
+docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
+
+# 删除之前的镜像
+echo "romove old image..."
+docker rmi $ImageFullName
+
+# 打包镜像
+echo "build image..."
+docker build -t $ImageFullName .
+
+# 打印当前镜像
+echo "current docker images..."
+docker images | grep $ImageFullName
+
+# 启动容器
+echo "start container..."
+docker run -d -p 9090:8080 --name $ImageName $ImageFullName
+
+# 打印当前容器
+echo "current container..."
+docker ps -a | grep $ImageName
 
 # 推送镜像
 echo "push image..."
-echo "Registry: $Registry"
-printf $DOCKER_USER_PSW | docker login -u $DOCKER_USER_USR --password-stdin $Registry
 printf $DOCKER_USER_PASSWORD | docker login -u $DOCKER_USER_USERNAME --password-stdin $Registry
-#docker push $ImageFullName:latest
+docker push $ImageFullName:latest
