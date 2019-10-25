27645c28e50a4bae18f0cf4a9fc56bbc4401d452
cmyiyi <cmygit@163.com>
2019-10-26 01:34:27 +0800

    change jenkins pipeline. 131

--- docker-build.sh
+++ docker-build.sh
@@ -2,34 +2,35 @@
 
 ImageFullName=$Registry/$NameSpace/$ImageName
 
-echo "docker build..."
-echo "ImageFullName: $ImageFullName"
-
-# 先删除之前的容器
-echo "remobe old container..."
-docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
-
-# 删除之前的镜像
-echo "romove old image..."
-docker rmi $ImageFullName
-
-# 打包镜像
-echo "build image..."
-docker build -t $ImageFullName .
-
-# 打印当前镜像
-echo "current docker images..."
-docker images | grep $ImageFullName
-
-# 启动容器
-echo "start container..."
-docker run -d -p 9090:8080 --name $ImageName $ImageFullName
-
-# 打印当前容器
-echo "current container..."
-docker ps -a | grep $ImageName
+#echo "docker build..."
+#echo "ImageFullName: $ImageFullName"
+#
+## 先删除之前的容器
+#echo "remobe old container..."
+#docker ps -a | grep $ImageName | awk '{print $1}'| xargs docker rm -f
+#
+## 删除之前的镜像
+#echo "romove old image..."
+#docker rmi $ImageFullName
+#
+## 打包镜像
+#echo "build image..."
+#docker build -t $ImageFullName .
+#
+## 打印当前镜像
+#echo "current docker images..."
+#docker images | grep $ImageFullName
+#
+## 启动容器
+#echo "start container..."
+#docker run -d -p 9090:8080 --name $ImageName $ImageFullName
+#
+## 打印当前容器
+#echo "current container..."
+#docker ps -a | grep $ImageName
 
 # 推送镜像
 echo "push image..."
-docker login -u $DOCKER_USER_USR -p $DOCKER_USER_PSW $Registry
+echo "Registry: $Registry"
+printf $DOCKER_USER_PSW | docker login -u $DOCKER_USER_USR --password-stdin  $Registry
 docker push $ImageFullName:latest
